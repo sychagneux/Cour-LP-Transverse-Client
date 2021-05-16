dd9808dfdc9aacedf6d6fa0e91b4ccf5f4b581b5
Pierre Pic <pierre.pic63122@gmail.com>
2021-05-16 23:28:21 +0200

    add notification ask permission

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -35,4 +35,10 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+// Ask for notification
+Notification.requestPermission( function(status) {
+  console.log(status); // les notifications ne seront affichées que si "autorisées"
+  var n = new Notification("title", {body: "notification body"}); // this also shows the notification
+});
+
 serviceWorker.register();
